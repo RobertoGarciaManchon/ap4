3057b29c669e7a7f613110a9d645e18bf25d57c4
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-10 19:59:19 +0100

    Añadido un comentario nuevo

---
+++ saludo/src/saludo/Saludo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saludo;
+
+/**
+ *
+ * @author Roberto
+ */
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int a=5;
+      int b=2;
+      int suma=a+b;
+      int resta=a-b;
+      int div=a/b;
+      System.out.println("La suma de a y b es "+suma);
+ 
+              
+    }
+    
+}
